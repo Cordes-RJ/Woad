6f62586ad5c56ca8c6bac442fcd13e1051c3b1f3
Cordes-RJ <richardj.cordes@gmail.com>
2019-09-18 21:12:23 -0400

    stashing

--- Code/scanParser.py
+++ Code/scanParser.py
@@ -3,13 +3,20 @@
 """
 parser is used to scan the auction data file for auction entries
 """
-
+import parameters as p
     
 class Parser:
-    def __init__(self):
+    def __init__(self, Parameters):
+        #
+        Parameters = p.Parameters()
+        #
+        self.p = Parameters
         self.rawList = []
     #find where entries begin
     def Parse(self, content):
+        self.parseRawTxt(content)
+        self.cleanList()
+    def parseRawTxt(self,content):
         #Find Start
         content = str(content[content.find("return {") + 9:len(content)])
         cont = True
@@ -23,3 +30,15 @@ class Parser:
         nextDelimit = content.find(",}", nextStart, len(content))
         self.rawList.append(str(content[nextStart:nextDelimit]))
         return str(content[nextDelimit:len(content)]), True
+    def checkWL(self,item):
+        if self.p.Check(int(str(item[0:item.find(":")]))):
+            return True
+        else:
+            return False
+    def cleanList(self):
+        i = 0
+        while i < len(self.rawList):
+            if self.checkWL(self.rawList[i]):
+                i+=1
+            else:
+                self.rawList.pop(i)
--- Code/scanParser_test.py
+++ Code/scanParser_test.py
@@ -4,13 +4,13 @@
 all files tested in console in the interest of efficiency
 """
 
-#import utility as util
-#import parameters as p
+import utility as util
+import parameters as p
 import scanParser as sps
 
-#Parameters = p.Parameters()
-parser = sps.Parser()
-parser.Parse("TestData/Auc-ScanData_short")
+Parameters = p.Parameters()
+parser = sps.Parser(Parameters)
+parser.Parse(util.Readin_bloc("TestData/Auc-ScanData_short.txt"))
 x = parser.rawList
 
 #%%
